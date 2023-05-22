65fac14ed9e238fde290348ad00fba09d55967d3
w0jd <dwoitek1@gmail.com>
2023-05-22 19:29:06 +0200

    połączenie z bazą dancyh
    aplikacja łączy się teraz z bazą danych

---
+++ projekt/projekt/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using projekt.Models;
+
+namespace Projekt.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+           
+        }
+        public DbSet<Produkt> Produkty { get; set; }
+    }
+}
