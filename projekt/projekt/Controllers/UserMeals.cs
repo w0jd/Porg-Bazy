9102a0a3d0bab73787cc2136c19a290d77607396
w0jd <dwoitek1@gmail.com>
2023-06-25 12:57:05 +0200

    dodanie wypisywania wszystkich posiłków użytkownika

---
+++ projekt/projekt/Controllers/UserMeals.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projekt.Controllers
+{
+    public class UserMealsController:Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserMealsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var currentDate = DateOnly.FromDateTime(DateTime.Now);
+            var userName = User.FindFirst(ClaimTypes.Name).Value;
+
+            var wyniki = _context.Konta
+                .Where(a => a.Nazwa == userName)
+                .Include(acc => acc.Jadlospisy)
+                .ThenInclude(jadlo => jadlo.Dania);
+            return View(wyniki);
+        }
+    }
+}
