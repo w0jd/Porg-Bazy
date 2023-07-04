5c28713e48e2a8e396437722804ceddb85197c0e
w0jd <dwoitek1@gmail.com>
2023-07-04 21:51:25 +0200

    usuwa po dwa takie same

--- projekt/projekt/Controllers/UserMealsController.cs
+++ projekt/projekt/Controllers/UserMealsController.cs
@@ -24,8 +24,39 @@ namespace projekt.Controllers
             jadlospis.IdKonta = wyniki.Id;
             _context.Add(jadlospis);
             _context.SaveChanges();
-            return View("../Account/Index");
+            return RedirectToAction("Index");
         }
+        public IActionResult DeleteDayMeal(int id) {
+            var userName = User.FindFirst(ClaimTypes.Name).Value;
+            var konto = _context.Konta.First(acc=>acc.Nazwa.Equals(userName)); 
+            DateOnly dateOnly = new DateOnly();
+            string dataString = TempData["Date"].ToString();
+            DateOnly.TryParse(dataString, out dateOnly) ;
+            /*            var wyniki = _context.Jadlospis.First(acc => acc.Dzień == dateOnly && acc.IdKonta ==konto.Id  && acc.IdDania == id);
+
+
+
+                        _context.Remove(wyniki);*/
+            var deleteQuery = @"
+    DELETE FROM Jadlospis
+    WHERE Dzień = {0}
+      AND IdKonta = (
+        SELECT k.Id
+        FROM Konta k
+        WHERE k.Nazwa = {1}
+      )
+      AND IdDania = {2}
+
+    LIMIT 1";
+
+
+
+            _context.Database.ExecuteSqlRaw(deleteQuery, dateOnly, userName, id);
+
+            _context.SaveChanges();
+            return View("../Home/Index");
+        }
+   
         public IActionResult AddExistingMeal(string id)
         {
            //DateOnly dat;
